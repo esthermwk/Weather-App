fe91824dd9b83d1f373298d4f59a048cad415ea5
esthermwk <esthermwk@gmail.com>
2022-07-05 10:54:12 +0100

    Initial commit

---
+++ js.js
@@ -0,0 +1,73 @@
+let h2 = document.querySelector("h2");
+let now = new Date();
+
+h2.innerHTML = `Last updated: ${now}`;
+
+function displayWeatherCondition(response) {
+  console.log(response.data);
+  document.querySelector("#cityName").innerHTML = response.data.name;
+  document.querySelector(".tempElement").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#high").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#low").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#feelsLike").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+}
+
+function searchCity(city) {
+  let apiKey = "caf5e0b506da44f3e8e5a592de514d36";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#result").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "caf5e0b506da44f3e8e5a592de514d36";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(".tempElement");
+  temperatureElement.innerHTML = 82;
+}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(".tempElement");
+  temperatureElement.innerHTML = 19;
+}
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+let weatherApp = document.querySelector(".weather-app");
+weatherApp.addEventListener("submit", handleSubmit);
+
+let currentLocation = document.querySelector("#current-location-btn");
+currentLocation.addEventListener("click", getCurrentLocation);
+
+searchCity("Hong Kong");
