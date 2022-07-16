fe4cb0ff5c0223abb658138c97bbad8d769753c9
esthermwk <esthermwk@gmail.com>
2022-07-16 12:07:11 +0100

    Forecast added

--- js.js
+++ js.js
@@ -3,24 +3,41 @@ let now = new Date();
 
 h2.innerHTML = `Last updated: ${now}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weather-forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
             <div class="col-2">
-             <div class="forecast-date"> ${day}</div>
-              <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weatherIcon" width="72"/> 
+             <div class="forecast-date"> ${formatDay(forecastDay.dt)}</div>
+          
+             <img src="http://openweathermap.org/img/wn/${
+               forecastDay.weather[0].icon
+             }@2x.png" alt="weatherIcon" width="72"/> 
               <div class="forecast-temp">
-                <span class="max-temp">25</span>° <span class="min-temp">20</span>° 
+                <span class="max-temp">${Math.round(
+                  forecastDay.temp.max
+                )}</span>° <span class="min-temp">${Math.round(
+          forecastDay.temp.min
+        )}</span>° 
               </div>
             </div>
     `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
